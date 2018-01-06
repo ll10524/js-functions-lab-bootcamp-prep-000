205aef1ebaf06d1c5ab9331830323fdb0862c13e
Leo Tu <ll410119@gmail.com>
2018-01-06 21:55:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
-function holidayCountdown() {
-  
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`;
 }
